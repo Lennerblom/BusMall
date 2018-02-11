3a49142565593714142bfdd9863448a3e05b91b5
michael lennerblom <mlennerblom@gmail.comgit config --listgit config --global user.name Michael Lennerblomgit config --global user.email mlennerblom@gmail.comgit config --listgit config --global user.name Michael>
2018-02-10 19:35:54 -0800

    done for today I guess, still working on counter, no duplicates on consecutive clicks and styling

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 ImageGetter.allPics = [];
+var totalClicks = 0;
 
 function ImageGetter(name, filepath) {
   this.name = name;
@@ -29,18 +30,23 @@ new ImageGetter('water-can', 'img/water-can.jpg');
 new ImageGetter('wine', 'img/wine-glass.jpg');
 
 console.log(ImageGetter.allPics.length);
+for(var i = 0; i < ImageGetter.length; i++){
+  var imageName = [ImageGetter.name[i]];
+}
+console.log(imageName);
 
 var img1El = document.getElementById('pic1');
 var img2El = document.getElementById('pic2');
 var img3El = document.getElementById('pic3');
 
+
+//GLOBAL RANDOM NUMBER GENERATOR
+function randomIndex() {
+  var ranNum = Math.floor(Math.random() * ImageGetter.allPics.length);
+  return ranNum;
+}
 function randomPic() {
-
-  function randomIndex() {
-    var ranNum = Math.floor(Math.random() * ImageGetter.allPics.length);
-    return ranNum;
-  }
-
+//LOCAL RANDOM 1ST, 2ND, 3RD
   var randNum1 = randomIndex();
   console.log(randNum1);
 
@@ -56,14 +62,38 @@ function randomPic() {
   }
   console.log(randNum3);
 
+  var olEl = document.getElementById('researchLog');
+  var liOneEl = document.createElement('li');
+  var liTwoEl = document.createElement('li');
+  var liThreeEl = document.createElement('li');
+
+  //SETTING RANDOM IMAGES TO DISPLAY AND ADDING COUNTER
   img1El.src = ImageGetter.allPics[randNum1].filepath;
+  liOneEl = ImageGetter.allPics[randNum1].name;
+  //olEl.appendChild(liOneEl);
+  totalClicks = ImageGetter.allPics[randNum1].clicks++;
   console.log(img1El.src);
+  console.log(liOneEl);
+  console.log(totalClicks);
 
   img2El.src = ImageGetter.allPics[randNum2].filepath;
+  liTwoEl = ImageGetter.allPics[randNum2].name;
+  totalClicks = ImageGetter.allPics[randNum2].clicks++;
   console.log(img2El.src);
+  console.log(liTwoEl);
+  console.log(totalClicks);
 
   img3El.src = ImageGetter.allPics[randNum3].filepath;
+  liThreeEl = ImageGetter.allPics[randNum3].name;
+  totalClicks = ImageGetter.allPics[randNum3].clicks++;
   console.log(img3El.src);
+  console.log(liThreeEl);
+  console.log(totalClicks);
+
+  //for(var i = 0; i < 25; i++) {
+  ImageGetter.clicks = parseInt(ImageGetter.clicks++);
+  console.log(ImageGetter.clicks);
+  //}
 }
 randomPic();
 
@@ -71,6 +101,13 @@ img1El.addEventListener('click', randomPic);
 img2El.addEventListener('click', randomPic);
 img3El.addEventListener('click', randomPic);
 
+totalClicks += ImageGetter.clicks;
+
+if(totalClicks >= 25) {
+  img1El.removeEventListener('click', randomPic);
+  img2El.removeEventListener('click', randomPic);
+  img3El.removeEventListener('click', randomPic);
+}
 /*ImageGetter.clicks += 1;
   //console.log(randomIndex);
   img2El.src = ImageGetter.allPics[randomIndex2].filepath;
